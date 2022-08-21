3bf1c77abf38141d858234fe002e135847a213af
ISAAC LEE <adsdfsd2321@naver.com>
2022-08-21 20:24:54 +0900

    refactor: util/riothttpclient -> domain/riothttpclient

--- domain/src/main/kotlin/info/isaaclee/lolgoitne/domain/modules/RiotHttpClient.kt
+++ domain/src/main/kotlin/info/isaaclee/lolgoitne/domain/modules/RiotHttpClient.kt
@@ -1,9 +1,43 @@
 package info.isaaclee.lolgoitne.domain.modules
 
 import info.isaaclee.lolgoitne.domain.bot.dao.*
+import info.isaaclee.lolgoitne.domain.bot.exceptions.*
+import info.isaaclee.lolgoitne.util.http.HttpClient
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.bodyToMono
 
-interface RiotHttpClient {
-	fun getSummonerInfo(nickname: String): SummonerDAO
+@Component
+class RiotHttpClient(
+	environment: Environment
+): HttpClient("https://kr.api.riotgames.com/lol") {
+	private val apiToken: String = environment.getRequiredProperty("riot.apiKey")
 
-	fun getGameInfo(summonerId: String): CurrentGameInfoDAO
+	fun getSummonerInfo(nickname: String): SummonerDAO {
+		return this.webClient.get()
+			.uri("/summoner/v4/summoners/by-name/${nickname}")
+			.header("X-Riot-Token", apiToken)
+			.exchangeToMono { response ->
+				if (response.rawStatusCode() == 404) {
+					throw UserNotFoundException()
+				}
+				response.bodyToMono<SummonerDAO>()
+			}
+			.blockOptional()
+			.get()
+	}
+
+	fun getGameInfo(summonerId: String): CurrentGameInfoDAO {
+		return this.webClient.get()
+			.uri("/spectator/v4/active-games/by-summoner/${summonerId}")
+			.header("X-Riot-Token", apiToken)
+			.exchangeToMono { response ->
+				if (response.rawStatusCode() == 404) {
+					throw GameNotFoundException()
+				}
+				response.bodyToMono<CurrentGameInfoDAO>()
+			}
+			.blockOptional()
+			.get()
+	}
 }
