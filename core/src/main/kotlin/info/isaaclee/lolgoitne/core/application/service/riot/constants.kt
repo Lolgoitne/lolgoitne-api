cd83e537d21b74b64496870797efd9e710b4b4b5
HYUNGJUN LEE <adsdfsd2321@naver.com>
2022-08-23 16:42:44 +0900

    remove: unused constants

--- core/src/main/kotlin/info/isaaclee/lolgoitne/core/application/service/riot/constants.kt
+++ core/src/main/kotlin/info/isaaclee/lolgoitne/core/application/service/riot/constants.kt
@@ -1,7 +1,3 @@
 package info.isaaclee.lolgoitne.core.application.service.riot
 
-const val GAME_NOT_FOUND = "game_not_found"
-
-const val USER_NOT_FOUND = "user_not_found"
-
 const val INTERNAL_SERVER_ERROR_MESSAGE = "현재 서버가 많이 아파요!"
